f725b239b7852bb8618a6b98b7f91254b8c466c7
이재윤 <154980427+JYL35@users.noreply.github.com>
2024-03-07 19:13:49 +0900

    Create solve.java

---
+++ Hash/11478번:서로 다른 부분 문자열의 개수/solve.java
@@ -0,0 +1,21 @@
+import java.io.*;
+import java.util.HashSet;
+
+public class Main {
+	static int n, m;
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		String s = br.readLine();
+		HashSet<String> set = new HashSet<>();
+		
+		for(int i = 0; i < s.length(); i++) {
+			for(int j = i + 1; j <= s.length(); j++) {
+				set.add(s.substring(i, j));
+			}
+		}
+		System.out.println(set.size());
+	}
+
+}
