92fe9f9f3e0ee4c8b009a81adc4c5e68363d7048
SunM <coex0113@gmail.com>
2024-10-26 23:31:21 +0900

    Feat. add 2668.cpp file in Graph repo

---
+++ Graph/2668번 - 숫자고르기/2668.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int n;
+int arr[2][101];
+vector<int> res;
+
+void input() {
+    cin >> n;
+    for(int i = 1; i <= n; i++) {
+        int tmp;
+        cin >> tmp;
+        arr[1][i] = tmp;
+    }
+}
+
+void recursive(int st, int val) {
+    if(arr[0][val]) {
+        if(st == val) res.push_back(st);
+        return;
+    }
+    arr[0][val] = 1;
+    recursive(st, arr[1][val]);
+}
+
+void solve() {
+    for(int i = 1; i <= n; i++) {
+        fill_n(&arr[0][0], 101, 0);
+        arr[0][i] = 1;
+        recursive(i, arr[1][i]);
+    }
+    cout << res.size() << '\n';
+    for(int &it : res) cout << it << '\n';
+}
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    
+    input();
+    solve();
+    
+    return 0;
+}
