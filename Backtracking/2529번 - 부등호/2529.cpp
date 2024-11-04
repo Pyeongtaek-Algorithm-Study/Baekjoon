ce76edafbf70212f305202dbb452ef4150d2fe42
SunM <coex0113@gmail.com>
2024-11-04 22:02:57 +0900

    Feat. add 2529.cpp file

---
+++ Backtracking/2529번 - 부등호/2529.cpp
@@ -0,0 +1,68 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int n;
+vector<char> k;
+string min_ans = "987654321";
+string max_ans = "0";
+
+void input() {
+    cin >> n;
+    for(int i = 0; i < n; i++) {
+        char tmp;
+        cin >> tmp;
+        k.push_back(tmp);
+    }
+}
+
+void recursive(string num, int arr[10]) {
+    int len = num.length() - 1;
+    if(len >= n) {
+        min_ans = min(min_ans, num);
+        max_ans = max(max_ans, num);
+        return;
+    }
+    
+    for(int i = 0; i < 10; i++) {
+        if(arr[i]) continue;
+        
+        if(k[len] == '>' && num[len] - '0' > i) {
+            num += to_string(i);
+            arr[i] = 1;
+            recursive(num, arr);
+            arr[i] = 0;
+            num = num.substr(0, num.length() - 1);
+        }
+        else if(k[len] == '<' && num[len] - '0' < i) {
+            num += to_string(i);
+            arr[i] = 1;
+            recursive(num, arr);
+            arr[i] = 0;
+            num = num.substr(0, num.length() - 1);
+        }
+    }
+}
+
+void solve() {
+    for(int i = 0; i < 10; i++) {
+        string s = to_string(i);
+        int chk[10] = { 0, };
+        chk[i] = 1;
+        recursive(s, chk);
+    }
+    
+    cout << max_ans << '\n';
+    cout << min_ans << '\n';
+}
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    
+    input();
+    solve();
+    
+    return 0;
+}
