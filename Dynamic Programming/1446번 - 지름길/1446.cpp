caa8eba2c36e973b44d0fa9a9e80e01a73537d91
SunM <coex0113@gmail.com>
2025-03-30 14:42:46 +0900

    Feat: add 1446 solve code

---
+++ Dynamic Programming/1446번 - 지름길/1446.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+#define INF 1e9
+
+int n, d;
+vector<vector<int> > v;
+int dp[10001] = { 0, };
+
+void input() {
+    cin >> n >> d;
+    for(int i = 0, a, b, c; i < n; i++) {
+        cin >> a >> b >> c;
+        v.push_back({b, a, c});
+    }
+}
+
+bool cmp(vector<int> v1, vector<int> v2) {
+    return v1[0] < v2[0];
+}
+
+void solve() {
+    for(int i = 0; i <= d; i++) dp[i] = i;
+    
+    sort(v.begin(), v.end(), cmp);
+    
+    for(auto &it : v) {
+        dp[it[0]] = min(dp[it[0]], dp[it[1]] + it[2]);
+        
+        for(int j = it[1] + 1; j <= d; j++) {
+            dp[j] = min(dp[j], dp[j - 1] + 1);
+        }
+    }
+    
+    cout << dp[d] << '\n';
+}
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    
+    input();
+    solve();
+    
+    return 0;
+}
---
+++ Dynamic Programming/1446번 - 지름길/1446.py
@@ -0,0 +1,62 @@
+import sys
+import heapq
+
+input = sys.stdin.readline
+
+INF = float('inf')
+n, d = map(int, input().split())
+short = [list(map(int, input().split())) for _ in range(n)]
+
+def dijkstra():
+    # 최단 거리 초기 설정
+    weights = [INF] * (d + 1)
+    
+    # 각 경로의 초기 설정
+    edges = [[] for _ in range(d + 1)]
+    for i in range(d):
+        edges[i].append([i + 1, 1]) 
+        
+    # 입력받은 지름길 추가
+    for li in short:
+        if li[1] > d:
+            continue
+        edges[li[0]].append([li[1], li[2]])
+    
+    # dijkstra 알고리즘    
+    pq = []
+    heapq.heappush(pq, [0, 0])
+    weights[0] = 0
+    
+    while pq:
+        src, cost = heapq.heappop(pq)
+        
+        if weights[src] < cost:
+            continue
+        
+        for edge in edges[src]:
+            if weights[edge[0]] > weights[src] + edge[1]:
+                weights[edge[0]] = weights[src] + edge[1]
+                heapq.heappush(pq, edge)
+        
+    return weights[d]
+
+def solve():
+    sorted_short = sorted(short, key = lambda x : x[1])
+    dp = list(range(d + 1))
+    
+    for li in sorted_short:
+        src, des, cost = li
+        if des > d:
+            continue
+        
+        dp[des] = min(dp[des], dp[src] + cost)
+        
+        for j in range(src + 1, d + 1):
+            dp[j] = min(dp[j], dp[j - 1] + 1)
+    
+    print(dijkstra())
+    # print(dp[d])
+    
+
+if __name__ == "__main__":
+    solve()
